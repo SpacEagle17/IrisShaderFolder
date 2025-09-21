649dcbc90c49cc0e877c51c560f8781120a81b63
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2025-09-21 18:54:15 +0200

    forge

--- src/main/java/com/spaceagle17/iris_shader_folder/IrisShaderFolder.java
+++ src/main/java/com/spaceagle17/iris_shader_folder/IrisShaderFolder.java
@@ -1,12 +1,12 @@
 package com.spaceagle17.iris_shader_folder;
 
-import net.neoforged.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Mod("iris_shader_folder")
 public class IrisShaderFolder {
-    public static final String MOD_ID = "IrisShaderFolder";
+    public static final String MOD_ID = "iris_shader_folder";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public IrisShaderFolder() {
