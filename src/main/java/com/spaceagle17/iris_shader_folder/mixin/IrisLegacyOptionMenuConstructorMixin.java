90a67ffce0347f33d25e6e44e3bee59a46cb5500
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2025-09-27 01:51:36 +0200

    finalize as well

---
+++ src/main/java/com/spaceagle17/iris_shader_folder/mixin/IrisLegacyOptionMenuConstructorMixin.java
@@ -0,0 +1,32 @@
+package com.spaceagle17.iris_shader_folder.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.spaceagle17.iris_shader_folder.ShaderRecolorSystem;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Pseudo
+@Debug(export = true)
+@Mixin(targets = "net.coderbot.iris.gui.element.widget.OptionMenuConstructor", remap = false)
+public class IrisLegacyOptionMenuConstructorMixin {
+
+    /**
+     * Modifies the expression value of getCurrentPackName() within the lambda method
+     */
+    @ModifyExpressionValue(
+        method = "lambda$static$3(Lnet/coderbot/iris/shaderpack/option/menu/OptionMenuMainElementScreen;)Lnet/coderbot/iris/gui/element/screen/ElementWidgetScreenData;",
+        at = @At(
+            value = "INVOKE", 
+            target = "Lnet/coderbot/iris/Iris;getCurrentPackName()Ljava/lang/String;",
+            remap = false
+        ),
+        remap = false
+    )
+    private static String modifyPackNameValue(String originalPackName) {
+        String recoloredName = ShaderRecolorSystem.getInstance().recolorShaderName(originalPackName);
+        System.out.println("[IrisShaderFolder] Recolored pack name: " + originalPackName + " -> " + recoloredName);
+        return recoloredName;
+    }
+}
---
+++ src/main/java/com/spaceagle17/iris_shader_folder/mixin/IrisModernOptionMenuConstructorMixin.java
@@ -0,0 +1,30 @@
+package com.spaceagle17.iris_shader_folder.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.spaceagle17.iris_shader_folder.ShaderRecolorSystem;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Pseudo
+@Debug(export = true)
+@Mixin(targets = "net.irisshaders.iris.gui.element.widget.OptionMenuConstructor", remap = false)
+public class IrisModernOptionMenuConstructorMixin {
+
+    /**
+     * Modifies the expression value of getCurrentPackName() within the lambda method
+     */
+    @ModifyExpressionValue(
+        method = "lambda$static$0(Lnet/irisshaders/iris/shaderpack/option/menu/OptionMenuMainElementScreen;)Lnet/irisshaders/iris/gui/element/screen/ElementWidgetScreenData;",
+        at = @At(
+            value = "INVOKE", 
+            target = "Lnet/irisshaders/iris/Iris;getCurrentPackName()Ljava/lang/String;",
+            remap = false
+        ),
+        remap = false
+    )
+    private static String modifyPackNameValue(String originalPackName) {
+        String recoloredName = ShaderRecolorSystem.getInstance().recolorShaderName(originalPackName);
+        System.out.println("[IrisShaderFolder] Recolored pack name: " + originalPackName + " -> " + recoloredName);
+        return recoloredName;
+    }
+}
