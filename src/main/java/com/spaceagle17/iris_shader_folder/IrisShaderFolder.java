340f11a7538297ec31add6a408ec9d3ab51f7a2a
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2025-10-01 20:15:56 +0200

    1.2.0

--- src/main/java/com/spaceagle17/iris_shader_folder/IrisShaderFolder.java
+++ src/main/java/com/spaceagle17/iris_shader_folder/IrisShaderFolder.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class IrisShaderFolder implements ModInitializer {
     public static final String MOD_ID = "iris_shader_folder";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.2.0";
 
     public static Path shaderpacks = FabricLoader.getInstance().getGameDir().resolve("shaderpacks");
     private static IrisShaderFolder INSTANCE;
