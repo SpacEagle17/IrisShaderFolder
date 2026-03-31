389d4bbf465aff24048f444e052096540bf6ade8
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2026-03-31 19:59:25 +0200

    another temp

--- src/main/java/com/spaceagle17/iris_shader_folder/mixin/legacy/IrisLegacyShaderEntryMixin.java
+++ src/main/java/com/spaceagle17/iris_shader_folder/mixin/legacy/IrisLegacyShaderEntryMixin.java
@@ -1,8 +1,8 @@
 package com.spaceagle17.iris_shader_folder.mixin.legacy;
 
 import com.spaceagle17.iris_shader_folder.IrisShaderFolder;
-import com.spaceagle17.iris_shader_folder.ShaderRecolorSystem;
 import com.spaceagle17.iris_shader_folder.ShaderTooltipSystem;
+import com.spaceagle17.iris_shader_folder.util.ShaderName;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -62,7 +62,7 @@ public class IrisLegacyShaderEntryMixin {
         require = 0
     )
     private String modifyNameVariable(String name) {
-        String recoloredName = ShaderName.renameShader(originalPackName);
+        String recoloredName = ShaderName.renameShader(name);
         this.currentShaderName = name;
         this.currentShaderNameRecolored = recoloredName;
         return recoloredName;
