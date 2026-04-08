6d1202d4f6f3a84e65822c3a28287b126bd9a342
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2026-04-08 13:00:55 +0200

    correct isSpacEagle

--- src/main/java/com/spaceagle17/iris_shader_folder/IrisShaderFolder.java
+++ src/main/java/com/spaceagle17/iris_shader_folder/IrisShaderFolder.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @Mod("iris_shader_folder")
 public class IrisShaderFolder {
@@ -224,7 +225,7 @@ public class IrisShaderFolder {
 
     public static boolean isSpacEagle() {
         try {
-            boolean containsSpacEagle = shaderpacks.toString().contains("SpacEagle");
+            boolean containsSpacEagle = shaderpacks.toString().toLowerCase(Locale.ROOT).contains("spaceagle");
             Path euphoriaFolder = shaderpacks.resolve("Euphoria-Patches");
             boolean hasEuphoriaFolder = Files.exists(euphoriaFolder) && Files.isDirectory(euphoriaFolder);
             return containsSpacEagle && hasEuphoriaFolder;
