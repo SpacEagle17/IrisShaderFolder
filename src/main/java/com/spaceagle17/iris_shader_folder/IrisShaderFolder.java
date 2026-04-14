4fd48b3ee31384d6e0ecbfc6fd1bc8c5640be6a1
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2026-04-14 12:37:46 +0200

    1.3.1

--- src/main/java/com/spaceagle17/iris_shader_folder/IrisShaderFolder.java
+++ src/main/java/com/spaceagle17/iris_shader_folder/IrisShaderFolder.java
@@ -15,7 +15,7 @@ import java.util.Locale;
 public class IrisShaderFolder {
     public static final String MOD_ID = "iris_shader_folder";
     public static final Logger LOGGER = LogManager.getLogger();
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.3.1";
 
     public static Path shaderpacks = FMLPaths.GAMEDIR.get().resolve("shaderpacks");
     private static IrisShaderFolder INSTANCE;
