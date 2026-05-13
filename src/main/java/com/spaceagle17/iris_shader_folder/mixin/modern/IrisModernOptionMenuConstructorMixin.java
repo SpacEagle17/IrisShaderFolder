653851180f6ae1a181e0e9b38b3b5c078d1ea352
SpacEagle17 <46494907+SpacEagle17@users.noreply.github.com>
2026-05-13 10:25:16 +0200

    remove warning

--- src/main/java/com/spaceagle17/iris_shader_folder/mixin/modern/IrisModernOptionMenuConstructorMixin.java
+++ src/main/java/com/spaceagle17/iris_shader_folder/mixin/modern/IrisModernOptionMenuConstructorMixin.java
@@ -1,7 +1,6 @@
 package com.spaceagle17.iris_shader_folder.mixin.modern;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.spaceagle17.iris_shader_folder.IrisShaderFolder;
 import com.spaceagle17.iris_shader_folder.util.ShaderName;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
